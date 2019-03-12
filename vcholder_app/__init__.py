fc26a1d245e56583944a5c6de98ec19be380930d
Eugene M <ujin@i.ua>
2019-03-12 12:19:18 +0200

    refactoring

--- vcholder_app/__init__.py
+++ vcholder_app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_uuid import FlaskUUID
 
 
 app = Flask(__name__)
+FlaskUUID(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vcholder.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['API_KEY'] = '1234567890'
@@ -14,5 +17,3 @@ import vcholder_app.models
 
 # db.drop_all()
 db.create_all()
-
-
--- vcholder_app/routes.py
+++ vcholder_app/routes.py
@@ -1,10 +1,12 @@
 from vcholder_app import app
+from vcholder_app import db
+from flask import render_template, url_for
 from flask import jsonify
 from flask import request
 from flask import abort
 from flask import Response
 from functools import wraps
-from vcholder_app.vcards import VCards
+from vcholder_app.models import VCard
 
 
 def require_appkey(view_function):
@@ -18,30 +20,41 @@ def require_appkey(view_function):
     return decorated_function
 
 
-# @app.route('/')
+def ldap_sync(uid, data):
+    updated = 0
+    inserted = 0
+    for vc_property in data:
+        vcard = VCard.query.filter_by(uid=uid, vc_property=vc_property).first()
+        if vcard:
+            if vcard.vc_value != data[vc_property]:
+                # print('Update: [%s] [%s]' % (vcard.vc_value, data[vc_property]))
+                vcard.vc_value = data[vc_property]
+                updated += 1
+        else:
+            db.session.add(VCard(uid, vc_property, data[vc_property]))
+            inserted += 1
+            # print('Insert')
+    db.session.commit()
+    return updated, inserted
+
+
+def render_vcf(items, uid):
+    vcl = ['BEGIN:VCARD', 'VERSION:3.0']
+    for vc_item in items:
+        vcl.append('%s:%s' % (vc_item.vc_property, vc_item.vc_value))
+    # vcl.append('UID:%s' % uid)
+    vcl.append('END:VCARD')
+    headers = {'Content-Disposition': 'inline; filename="%s.vcf"' % str(uid)}
+    return Response("\n".join(vcl), mimetype="text/x-vcard", headers=headers)
+
+
 @app.route('/index')
 def index():
-    return VCards().get_by_uid('00000000-0000-0000-0000-000000000000', True)
-
-
-@app.route('/<string:uid>', methods=['GET'])
-@app.route('/api/v1.0/get/<string:uid>', methods=['GET'])
-def get_vcard(uid):
-    if len(uid) == 0:
+    uid = '00000000-0000-0000-0000-000000000000'
+    items = VCard.query.filter_by(uid=uid).all()
+    if not items:
         abort(404)
-    if not VCards().is_uid(uid):
-        abort(404)
-    headers = {'Content-Disposition': 'inline; filename="%s.vcf"' % uid}
-    return Response(VCards().get_by_uid(uid), mimetype="text/x-vcard", headers=headers)
-
-
-@app.route('/api/v1.0/gettext/<string:uid>', methods=['GET'])
-def get_vcard_text(uid):
-    if len(uid) == 0:
-        abort(404)
-    if not VCards().is_uid(uid):
-        abort(404)
-    return VCards().get_by_uid(uid, True)
+    return render_template('vcard.html', uid=uid, vcard_items=items)
 
 
 @app.route('/api/v1.0/sync/<string:uid>', methods=['PUT'])
@@ -50,41 +63,40 @@ def sync_vcard(uid):
         abort(404)
     if not request.json:
         abort(400)
-    (u, i) = VCards().ldap_sync(uid, request.json)
+    (u, i) = ldap_sync(uid, request.json)
     # print(request.json)
     return jsonify({uid: {'updated': u, 'new': i}})
 
 
-# @app.route('/<string:uid>', methods=['GET'])
-@app.route('/api/v1.0/vcards/<string:uid>', methods=['GET'])
-def get_vcard2(uid):
-    if len(uid) == 0:
-        abort(404)
-    if not VCards().is_uid(uid):
+@app.route('/<uuid(strict=False):uid>', methods=['GET'])
+@app.route('/api/v1.0/vcards/<uuid(strict=False):uid>', methods=['GET'])
+def get_card(uid):
+    vcard_items = VCard.query.filter_by(uid=str(uid)).all()
+    if not vcard_items:
         abort(404)
     if request.args.get('html'):
-        return VCards().get_by_uid(uid, True)
-    headers = {'Content-Disposition': 'inline; filename="%s.vcf"' % uid}
-    return Response(VCards().get_by_uid(uid), mimetype="text/x-vcard", headers=headers)
+        return render_template('vcard.html', uid=str(uid),
+                               vcard_items=vcard_items,
+                               href=url_for('get_card', uid=str(uid)))
+    return render_vcf(vcard_items, str(uid))
 
 
-@app.route('/api/v1.0/vcards/<string:uid>', methods=['PUT'])
+@app.route('/api/v1.0/vcards/<uuid(strict=False):uid>', methods=['PUT'])
 @require_appkey
-def add_vcard(uid):
-    if len(uid) == 0:
-        abort(404)
-    if VCards().is_uid(uid):
+def add_card(uid):
+    if VCard.query.filter_by(uid=str(uid)).first():
         abort(409)
-    VCards().ldap_sync(uid, request.json)
+    ldap_sync(str(uid), request.json)
     return jsonify({uid: 'OK'})
 
 
-@app.route('/api/v1.0/vcards/<string:uid>', methods=['DELETE'])
+@app.route('/api/v1.0/vcards/<uuid(strict=False):uid>', methods=['DELETE'])
 @require_appkey
-def delete_vcard(uid):
-    if len(uid) == 0:
+def delete_card(uid):
+    vcards = VCard.query.filter_by(uid=str(uid)).all()
+    if not vcards:
         abort(404)
-    if not VCards().is_uid(uid):
-        abort(404)
-    VCards().delete_by_uid(uid)
+    for vcard in vcards:
+        db.session.delete(vcard)
+    db.session.commit()
     return jsonify({uid: 'OK'})
