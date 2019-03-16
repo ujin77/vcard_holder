7a8b329a5f90b1b5356aef451c9da5c30010b2e6
Eugene M <ujin@i.ua>
2019-03-16 13:00:48 +0200

    VCARD_SERVER_CONFIG

--- vcholder_app/__init__.py
+++ vcholder_app/__init__.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_uuid import FlaskUUID
 from flask_qrcode import QRcode
 from flask_login import LoginManager
-
+import uuid
 import os
 
-VCH_CONFIG = '%s.cfg' % __name__
+VCARD_SERVER_CONFIG = '%s.cfg' % __name__
 
 app = Flask(__name__)
 
@@ -17,10 +17,10 @@ login_manager = LoginManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vcholder.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['API_KEY'] = '1234567890'
-app.config['SECRET_KEY'] = '1234567890'
+app.config['SECRET_KEY'] = uuid.uuid4().hex
 app.config['AVATAR_FILE_TYPE'] = 'jpeg'
 app.config['DEFAULT_UUID'] = '00000000-0000-0000-0000-000000000000'
-app.config.from_pyfile(os.getenv('VCH_CONFIG', VCH_CONFIG), True)
+app.config.from_pyfile(os.getenv('VCARD_SERVER_CONFIG', VCARD_SERVER_CONFIG), True)
 
 db = SQLAlchemy(app)
 
